a5d0534d40415d66c811f4bd6134a730f944b5e9
jihyeon818 <ilove3531@naver.com>
2024-04-16 11:04:27 +0900

    target,inbody 데이터 저장, 아이디에 따른 정보 select,update 메소드 추가(MyPageDAO)

---
+++ DEVOPS-PROJECT1/src/db/InbodyData.java
@@ -0,0 +1,40 @@
+package db;
+
+public class InbodyData {
+	private String user_id;
+	private String inbody_cid;
+	private String inbody_weight;
+	private String inbody_muscle;
+	private String inbody_bodyfat;
+	
+	public String getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(String user_id) {
+		this.user_id = user_id;
+	}
+	public String getInbody_cid() {
+		return inbody_cid;
+	}
+	public void setInbody_cid(String inbody_cid) {
+		this.inbody_cid = inbody_cid;
+	}
+	public String getInbody_weight() {
+		return inbody_weight;
+	}
+	public void setInbody_weight(String inbody_weight) {
+		this.inbody_weight = inbody_weight;
+	}
+	public String getInbody_muscle() {
+		return inbody_muscle;
+	}
+	public void setInbody_muscle(String inbody_muscle) {
+		this.inbody_muscle = inbody_muscle;
+	}
+	public String getInbody_bodyfat() {
+		return inbody_bodyfat;
+	}
+	public void setInbody_bodyfat(String inbody_bodyfat) {
+		this.inbody_bodyfat = inbody_bodyfat;
+	}
+}
--- DEVOPS-PROJECT1/src/db/UsersData.java
+++ DEVOPS-PROJECT1/src/db/UsersData.java
@@ -4,9 +4,8 @@ public class UsersData {
 	private String user_id;
 	private String user_pw;
 	private String user_name;
-	private String user_ade;
+	private String user_age;
 	private String user_height;
-	private String user_weight;
 	private String user_gender;
 	
 	public String getUser_id() {
@@ -27,11 +26,11 @@ public class UsersData {
 	public void setUser_name(String user_name) {
 		this.user_name = user_name;
 	}
-	public String getUser_ade() {
-		return user_ade;
+	public String getUser_age() {
+		return user_age;
 	}
-	public void setUser_ade(String user_ade) {
-		this.user_ade = user_ade;
+	public void setUser_age(String user_age) {
+		this.user_age = user_age;
 	}
 	public String getUser_height() {
 		return user_height;
@@ -39,17 +38,10 @@ public class UsersData {
 	public void setUser_height(String user_height) {
 		this.user_height = user_height;
 	}
-	public String getUser_weight() {
-		return user_weight;
-	}
-	public void setUser_weight(String user_weight) {
-		this.user_weight = user_weight;
-	}
 	public String getUser_gender() {
 		return user_gender;
 	}
 	public void setUser_gender(String user_gender) {
 		this.user_gender = user_gender;
 	}
-	
 }
