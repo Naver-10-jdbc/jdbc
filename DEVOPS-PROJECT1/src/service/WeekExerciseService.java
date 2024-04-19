f0950522ff9adf7ca566133625fe25ee7bcef541
DESKTOP-OV23SEP\비트캠프 <ucs210212@naver.com>
2024-04-19 17:29:48 +0900

    실수...

--- DEVOPS-PROJECT1/src/service/WeekExerciseService.java
+++ DEVOPS-PROJECT1/src/service/WeekExerciseService.java
@@ -1,12 +1,13 @@
 package service;
 
 import view.diet.Diet;
+import view.week_ex.Week_Exercise;
 
 public class WeekExerciseService {
 	public void Exceute() {
 		System.out.println("WeekExerciseService È£Ãâ");
 		Thread thread=new Thread(()->{
-			new WeekExerciseService();
+			new Week_Exercise();
 		});
 		thread.start();
 	}
